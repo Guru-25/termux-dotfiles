6566c2fff7e6e9ce1e71b380c9ae1b03455a1013
Guru <guru@duck.com>
2023-07-28 22:20:55 +0530

    Update Session-Maker.py

--- .scripts/custom/Session-Maker.py
+++ .scripts/custom/Session-Maker.py
@@ -1,6 +1,6 @@
 from pyrogram import Client
 
-API_KEY = int(input("Enter API KEY: "))
-API_HASH = input("Enter API HASH: ")
+API_KEY = int(input("Enter api_id: "))
+API_HASH = input("Enter api_hash: ")
 with Client(name='user', in_memory=True, api_id=API_KEY, api_hash=API_HASH) as app:
     print(app.export_session_string())
