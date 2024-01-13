5d81dce7418100390a8e5627dcaebd29724923f2
Guru-25 <guru@duck.com>
2024-01-13 13:11:30 +0530

    new

--- .scripts/custom/kodi.sh
+++ .scripts/custom/kodi.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-termux-open https://mirrors.kodi.tv/releases/android/arm64-v8a/$"(curl -s https://github.com/xbmc/xbmc | grep "css-truncate css-truncate-target text-bold mr-2" | cut -d '"' -f 5 | cut -d '<' -f 1 | sed 's/>/kodi-/g')"-arm64-v8a.apk
+# Fetch the GitHub page HTML and extract the APK filename
+apk_filename=$(curl -s https://github.com/xbmc/xbmc | grep "css-truncate css-truncate-target text-bold mr-2" | cut -d '"' -f 5 | cut -d '<' -f 1 | sed 's/>v/kodi-/g')-arm64-v8a.apk
+
+# Construct the download URL
+download_url="https://mirrors.kodi.tv/releases/android/arm64-v8a/$apk_filename"
+
+# Use termux-open to download and open the APK
+termux-open $download_url
+
